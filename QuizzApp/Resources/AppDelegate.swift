52586754a2d45afe51911526a9568d409631b575
Borna Majstorovic <bmajstorovic8@gmail.com>
2020-04-10 11:56:40 +0200

    xibs

---
+++ QuizzApp/Resources/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  QuizzApp
+//
+//  Created by Borna on 09/04/2020.
+//  Copyright © 2020 hr.fer.majstorovic.borna. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let vc = InitialViewController()
+        
+        window?.rootViewController = vc
+        
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+
+}
+
---
+++ QuizzApp/ViewControllers/InitialViewController.swift
@@ -0,0 +1,31 @@
+//
+//  InitialViewController.swift
+//  QuizzApp
+//
+//  Created by Borna on 10/04/2020.
+//  Copyright © 2020 hr.fer.majstorovic.borna. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
